0e4117964520eee2c4d4745663def3556cceb210
Miguel Alves <miguelpalves01@gmail.com>
2024-08-30 11:49:26 -0300

    commit de todos arquivos

---
+++ src/books.js
@@ -0,0 +1,26 @@
+import img1 from './img/book-1.jpg'
+import img2 from './img/book-2.jpg'
+import img3 from './img/book-3.jpg'
+
+export const books = [
+  {
+    author: 'Colleen Hoover',
+    title: 'It Starts with Us',
+    img: img1,
+    id: 1,
+  },
+
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: img2,
+    id: 2,
+  },
+
+  {
+    author: 'Peter Brown',
+    title: 'The Wild Robot',
+    img: img3,
+    id: 3,
+  },
+]
